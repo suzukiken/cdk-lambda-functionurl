b2f8fddd52963709413efaf9cb37b9b1e6dd0e79
suzukiken <suzukiken@gmail.com>
2022-05-04 02:22:55 +0000

    change respnse

--- lambda/test.py
+++ lambda/test.py
@@ -1,5 +1,6 @@
 import logging
 import boto3
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -7,4 +8,20 @@ logger.setLevel(logging.DEBUG)
 def handler(event, context):
     logger.info(event)
     
-    return 'hello'
+    return json.dumps(
+        [
+            {
+                'id': 40481286914256,
+                'quantity': 2
+            },
+            {
+                'id': 42120466956496,
+                'quantity': 3
+            }
+        ]
+    )
+
+# 6837407678672
+# gid://shopify/ProductVariant/40481286914256
+# 7346082644176
+# gid://shopify/ProductVariant/42120466956496
